79c95c9f6bc9bbc4b0603934cdc45a3a34475a99
guiivale <guigato62@gmail.com>
2023-08-23 02:12:54 -0300

    Created enum for status and created method to store appointment WIP

---
+++ app/Enums/Status.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enums;
+
+class Status
+{
+    const SCHEDULED_ID = '1';
+    const PROGRESS_ID = '2';
+    const COMPLETED_ID = '3';
+    const CANCELED_ID = '4';
+    const WAITING_PAYMENT_ID = '5';
+}
--- app/Http/Controllers/AppointmentsController.php
+++ app/Http/Controllers/AppointmentsController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use App\Http\Requests\GetUserAppointmentsRequest;
 use App\Http\Requests\StoreAppointmentRequest;
 use App\Models\User;
-use App\Enums\UserTypes;
+use App\Enums\Status;
+use App\Models\Appointment;
+use App\Models\Status as ModelsStatus;
 use Illuminate\Http\Request;
 
 class AppointmentsController extends Controller
@@ -21,6 +23,27 @@ class AppointmentsController extends Controller
         $vehicle = $request->vehicle;
 
         $service = $request->service;
+
+        $appointment = Appointment::create([
+            'scheduled_datetime' => $data['scheduled_datetime'],
+            'notes' => $data['notes'],
+        ]);
+
+        $status = ModelsStatus::find(Status::WAITING_PAYMENT_ID);
+        
+        $appointment->user()->associate($customer);
+        $appointment->company()->associate($company);
+        $appointment->vehicle()->associate($vehicle);
+        $appointment->service()->associate($service);
+        $appointment->status()->associate($status);
+        
+        $appointment->save();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Appointment created successfully',
+            'appointment' => $appointment,
+        ]);
     }
 
     public function userAppointments(GetUserAppointmentsRequest $request)
